94ab1bba04447523168995d32adcfd07761f69ed
Mac-78 <Mac-78@qingguMac-078deMacBook-Pro.local>
2019-05-13 15:49:40 +0800

    finished

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -6,7 +6,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
+theta_s = theta;
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,7 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+theta(1) = theta(1) - alpha*sum(X*theta_s-y)/m;
+theta(2) = theta(2) - alpha*sum((X*theta_s-y).*X(:,2))/m;
+theta_s = theta;
 
 
 
